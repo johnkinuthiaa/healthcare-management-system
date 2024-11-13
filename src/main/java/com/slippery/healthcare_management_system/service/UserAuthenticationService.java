e50483f26fd1570842c1e3c8d87051abc9a0588a
johnkinuthiaa <kinuthiajohn122@gmail.com>
2024-11-14 01:27:17 +0300

    initial commit

--- src/main/java/com/slippery/healthcare_management_system/service/UserAuthenticationService.java
+++ src/main/java/com/slippery/healthcare_management_system/service/UserAuthenticationService.java
@@ -1,4 +1,9 @@
 package com.slippery.healthcare_management_system.service;
 
+import com.slippery.healthcare_management_system.model.User;
+
+import java.util.List;
+
 public interface UserAuthenticationService {
+    List<User> getAllUsers();
 }
--- src/main/java/com/slippery/healthcare_management_system/service/UserAuthenticationServiceImpl.java
+++ src/main/java/com/slippery/healthcare_management_system/service/UserAuthenticationServiceImpl.java
@@ -1,4 +1,21 @@
 package com.slippery.healthcare_management_system.service;
 
-public class UserAuthenticationServiceImpl {
+import com.slippery.healthcare_management_system.model.User;
+import com.slippery.healthcare_management_system.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserAuthenticationServiceImpl implements UserAuthenticationService{
+    private final UserRepository repository;
+
+    public UserAuthenticationServiceImpl(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return repository.findAll();
+    }
 }
